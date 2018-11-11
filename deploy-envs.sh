f79d35f9aa106e7fb11cdaca4e6292fccd19f82f
Sebox <39335650+box-seb@users.noreply.github.com>
2018-11-11 14:42:59 +1300

    Move var definition to exports

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -8,6 +8,10 @@ export AWS_DEFAULT_REGION=us-west-2b
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST=ec2-54-69-164-235.us-west-2.compute.amazonaws.com
 
+#AWS_ACCOUNT_NUMBER={} set in private variable
+export AWS_ECS_REPO_DOMAIN=$AWS_ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+
+
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 # optional: SERVICESTACK_LICENSE
