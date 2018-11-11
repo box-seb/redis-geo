dd412b4d6232c9f9984565babcedeafe465d0a4a
Sebox <39335650+box-seb@users.noreply.github.com>
2018-11-11 14:43:17 +1300

    Add more logging

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,13 @@ source ../deploy-envs.sh
 
 
 # Build process
+
+echo "Starting to build docker image $IMAGE_NAME"
+
 docker build -t $IMAGE_NAME ../
+
+echo "Starting to build docker image $IMAGE_NAME"
+
 docker tag $IMAGE_NAME $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
+
+echo "Labeled docker image $IMAGE_NAME"
